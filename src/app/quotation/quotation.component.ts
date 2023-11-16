12ebfbb38df0ea795feebbd2a0f3b379885158c1
golfsomphot <golf1991da@gmail.com>
2023-11-16 08:12:18 +0700

    update

--- src/app/quotation/quotation.component.ts
+++ src/app/quotation/quotation.component.ts
@@ -12,14 +12,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./quotation.component.css']
 })
 export class QuotationComponent implements OnInit {
-  constructor(private modal: NgbModal, private http: HttpClient, private va: varible, private router: Router) { }
+  constructor(private ngbmodal: NgbModal, private http: HttpClient, private va: varible, private router: Router) { }
 
-  // page: any = 1;
+
   product: any = [];
   selectedValue: string = '';
   Activequotation: any = {
     quocode: "",
-    bill: "",
+    billtoname: "",
     ship: "",
     billaddress: "",
     shipaddress: "",
@@ -36,9 +36,10 @@ export class QuotationComponent implements OnInit {
     empid: 0,
     empname: "",
     detail: [],
+    custid: 0,
   }
 
-  public Activeitemproduct: any = {
+  public Activequodetail: any = {
     quocode: "",
     item: 1,
     productcode: "",
@@ -49,16 +50,15 @@ export class QuotationComponent implements OnInit {
     vat: 0.00,
     total: 0.00,
     remark: "",
-
-
   };
-  productcode: any;
 
+
+  productcode: any;
   rowData = [];
   rowData2: any[] = [];
-
   customerlist: any[] = [];
   employeelist: any[] = [];
+  quotationdata: any;
 
   async ngOnInit() {
     this.getdata("");
@@ -67,39 +67,44 @@ export class QuotationComponent implements OnInit {
       if (page instanceof NavigationStart) {
         console.log("page.url", page.url);
         if (page.url != '/quotation') {
-          this.modal.dismissAll();
+          this.ngbmodal.dismissAll();
         }
       }
     });
   }
 
 
+  //ปุ่มค้นหาหลัก
   btnSearch(keyword: any) {
     this.getdata(keyword);
-  }
-
+  };
+  //ฟังชั่น ลบ item ใน detail
   deleteitem(item: any) {
-    console.log("item : ", item);
-    if (confirm("Are you sure to delete " + item.item + " " + item.productcode)) {
 
+    if (confirm("Are you sure to delete " + item.item + " " + item.productcode)) {
       var listdetail: any[] = this.Activequotation.detail;
-      var i = listdetail.findIndex(x => x.item == item.item);
-      listdetail.splice(i, 1);
+      this.Activequotation.amount -= item.amount;
+      this.Activequotation.vat -= item.vat;
+      this.Activequotation.total -= item.total;
+      console.log("listdetail : ", listdetail);
+      var remove = listdetail.findIndex(x => x.item == item.item);
+      listdetail.splice(remove, 1);
       var n = 1;
       listdetail.forEach(detail => {
         detail.item = n;
         n++;
       });
+
     }
 
 
-  }
-
+  };
+  //คำสั่ง ดึงข้อมูลจาก customer
   async getcustomer() {
     let pram = { tbname: "getcustomer" }
     try {
       var response = await this.va.getdata("getquotation", pram);
-      console.log("response: ", response);
+      // console.log("response: ", response);
       if (response.data.length > 0) {
         this.customerlist = response.data;
 
@@ -111,13 +116,13 @@ export class QuotationComponent implements OnInit {
     }
     return [];
 
-  }
-
+  };
+  //ดึงข้อมูลจาก employee
   async getemployee() {
     let pram = { tbname: "getemployee" }
     try {
       var response = await this.va.getdata("getquotation", pram);
-      console.log("response: ", response);
+      // console.log("response: ", response);
       if (response.data.length > 0) {
         this.employeelist = response.data;
 
@@ -129,13 +134,14 @@ export class QuotationComponent implements OnInit {
     }
     return [];
 
-  }
+  };
 
+  //คำสั่ง กดปิด modal
   closemodal_click(modal: any) {
 
     modal.dismiss();
     /* this.page = 1; */
-  }
+  };
 
   columnDefs: ColDef[] = [
     { field: 'quocode', headerName: "Quotation Code" },
@@ -146,8 +152,6 @@ export class QuotationComponent implements OnInit {
     { field: 'amount', headerName: "Amount" },
     { field: 'discount', headerName: "Discount" },
     { field: 'total', headerName: "Total" },
-
-
   ];
 
   columnDefs2: ColDef[] = [
@@ -156,48 +160,77 @@ export class QuotationComponent implements OnInit {
     { field: 'qty', headerName: "Unit", width: 70 },
     { field: 'unitprice', headerName: "Unit-price", width: 100 },
     { field: 'total', headerName: "Total", width: 70 },
-
-
-
   ];
 
-  open(content: any) {
-    this.modal.open(content, { size: 'lg' });
+  //ฟังชั่นในปุ่มเพิ่ม create
+  opencreate_click(content: any) {
+    this.ngbmodal.open(content, { size: 'lg' });
     this.getcustomer();
     this.getemployee();
     this.getproduct();
+    this.Activequotation = {
+      quocode: "",
+      billtoname: "",
+      ship: "",
+      billtoaddress: "",
+      shipaddress: "",
+      paymentterms: "",
+      remark: "",
+      amount: 0.00,
+      vat: 0.00,
+      total: 0.00,
+      discount: 0.00,
+      totalline: 0,
+      reqdate: "",
+      saleid: 0,
+      salename: "",
+      empid: 0,
+      empname: "",
+      detail: [],
+    }
     this.Activequotation.quocode = this.va.datetostring(new Date, 4);
     this.Activequotation.reqdate = this.va.datetostring(new Date, 8);
     this.Activequotation.empname = this.va.userdata.fullname;
     this.Activequotation.empid = this.va.userdata.empid;
 
-  }
 
+  };
+
+  //ฟังชั่นเมื่อมีการเปลี่ยนแปลงจำนวน
+  Item_Change() {
+
+    this.Activequodetail.amount = this.Activequodetail.qty * this.Activequodetail.unitprice;
+    if (this.Activequodetail.discount > this.Activequodetail.amount) {
+      this.Activequodetail.discount = this.Activequodetail.amount;
+    }
+    this.Activequodetail.vat = (this.Activequodetail.amount - this.Activequodetail.discount) * 0.07;
+
+    this.Activequodetail.total = (this.Activequodetail.amount - this.Activequodetail.discount) + this.Activequodetail.vat;
+
+    console.log("Item_Change Activequodetail:", this.Activequodetail);
+  };
+
+  //ฟังชั่น เมื่อมีการเปลี่ยนแปลง  ลูกค้า
   customerchange(event: any) {
-
-
     var data = event.target.value;
     console.log("data : ", data);
-
-
     var selectdata = this.customerlist.find(x => x.custid == data);
     if (selectdata != undefined) {
       this.Activequotation.custid = selectdata.custid;
-      this.Activequotation.bill = selectdata.custname;
-      this.Activequotation.billaddress = selectdata.custaddress;
+      this.Activequotation.billtoname = selectdata.custname;
+      this.Activequotation.billtoaddress = selectdata.custaddress;
     } else {
       this.Activequotation.custid = 0;
-      this.Activequotation.bill = "";
-      this.Activequotation.billaddress = "";
+      this.Activequotation.billtoname = "";
+      this.Activequotation.billtoaddress = "";
     }
-  }
+  };
 
+  //ฟังชั่นเมื่อมีการเปลี่ยนแปลง พนักงาน  
   employeechange(event: any) {
 
     var data = event.target.value;
-    console.log("data : ", data);
-
-
+    // console.log("data : ", data);
     var selectdata = this.employeelist.find(x => x.empid == data);
     if (selectdata != undefined) {
       this.Activequotation.saleid = selectdata.empid;
@@ -207,20 +240,76 @@ export class QuotationComponent implements OnInit {
       this.Activequotation.saleid = 0;
       this.Activequotation.salename = "";
     }
-  }
+  };
 
+  //ปุ่ม Add Item
   btnadditem_click(contentadditem: any) {
-    this.modal.open(contentadditem, { size: 'none' });
+    this.ngbmodal.open(contentadditem, { size: 'none' });
+    this.Activequodetail = {
+      quocode: "",
+      item: 1,
+      productcode: "",
+      qty: 1,
+      unitprice: 0.00,
+      amount: 0,
+      discount: 0,
+      vat: 0.00,
+      total: 0.00,
+      remark: "",
+    };
+    this.Activequodetail.quocode = this.Activequotation.quocode;
+  };
+  //ฟังชั่น คลิ๊ก row
+  async rowDoubleClicked(event: any, contentview: any) {
+    this.ngbmodal.open(contentview, { size: 'lg' });
+    // console.log("event:",event);
+    this.quotationdata = event.data;
+    var quocode = this.quotationdata.quocode;
+    this.quotationdata.detail = await this.getquotationdetail(quocode);
+    console.log("quotationdata:", this.quotationdata);
+  };
 
-  }
+  //ฟังชั่น เรียก quotationdetail
+  async getquotationdetail(qid: string) {
+    let pram = { tbname: "getquodetail", quocode: qid }
+    try {
+      var response = await this.va.getdata("getquodetail", pram);
 
-  //api ต่างๆ
+      if (response.data.length > 0) {
+        return response.data;
+
+      } else {
+        return [];
+      }
+    } catch (ex) {
+
+    }
+  };
+
+  //ฟังชั่น เรียก quotationdetail
+  // async getquodetail() {
+  //   let pram = { tbname: "getquodetail" }
+  //   try {
+  //     var response = await this.va.getdata("getquodetail", pram);
+
+  //     if (response.data.length > 0) {
+  //       this.rowData2 = response.data;
+
+  //     } else {
+  //       alert("ไม่พบข้อมูล");
+  //     }
+  //   } catch (ex) {
+
+  //   }
+  // };
+
+  //ฟังชั่น เรียกข้อมูล  quotation
   async getdata(key: string) {
     let pram = { tbname: "getquotation", keyword: key }
     try {
       var response = await this.va.getdata("getquotation", pram);
-      console.log("getdata response: ", response);
-      if (response.data.length > 0 && response.code == '000') {
+
+      if (response.data.length > 0) {
         this.rowData = response.data;
 
       } else {
@@ -228,14 +317,15 @@ export class QuotationComponent implements OnInit {
       }
     } catch (ex) {
 
-    };
-  }
+    }
+  };
 
+  //ฟังชั่น เรียกข้อมูล  product
   async getproduct() {
     let pram = { tbname: "getproduct" }
     try {
       var response = await this.va.getdata("getquotation", pram);
-      console.log("getproduct response : ", response);
+
       if (response.data.length > 0) {
         this.product = response.data;
 
@@ -245,73 +335,71 @@ export class QuotationComponent implements OnInit {
     } catch (ex) {
 
     }
-  }
+  };
 
-  async getquodetail() {
-    let pram = { tbname: "getquodetail" }
-    try {
-      var response = await this.va.getdata("getquodetail", pram);
-      console.log("response: ", response);
-      if (response.data.length > 0) {
-        this.rowData2 = response.data;
-
-      } else {
-        alert("ไม่พบข้อมูล");
-      }
-    } catch (ex) {
-
+  // ฟังชั่น Save detail
+  async savequodetail(qdata: any) {
+    let pram = { tbname: "insertquodetail", data: qdata, uid: this.va.userdata.empid };
+    var quodetail = await this.http.post<any>("http://localhost:9000/insertquodetail", pram).toPromise();
+    console.log("quodetail", quodetail)
+    if (quodetail.code == "000" && quodetail.msg == "OK") {
+      return true;
+    } else {
+      return false;
     }
-  }
-
+  };
+  //ฟังชั่น InSertquotation +
   async insertquotation(modal: any) {
     var response;
     try {
       let pram = { tbname: "insertquotation", data: this.Activequotation, uid: this.va.userdata.empid };
-      console.log("pram: ", pram);
-      response = await this.http.post<any>("http://localhost:3000/quoinsert", pram).toPromise();
-      if (response.code == "000" && response.msg == "OK") {
+      var insertquotation = await this.http.post<any>("http://localhost:9000/quoinsert", pram).toPromise();
+
+      if (insertquotation.code == "000") {
+
+        for (var i = 0; i < this.Activequotation.detail.length; i++) {
+          var qdata = this.Activequotation.detail[i];
+          // console.log("qdata",qdata);
+          qdata.quocode = this.Activequotation.quocode;
+
+          if (await this.savequodetail(qdata)) {
+            alert("บันทึก quotation ไม่สำเร็จ กรุณาตรวจสอบใหม่อีกครั้ง");
+            return;
+          }
+        }
         alert("บันทึกสำเร็จ");
         modal.close();
       } else {
-        alert("บันทึกไม่สำเร็จ กรุณาตรวจสอบใหม่อีกครั้ง");
+        alert("บันทึก quotation ไม่สำเร็จ กรุณาตรวจสอบใหม่อีกครั้ง");
       }
-      //
-
     } catch (ex: any) {
 
     }
     return;
-  }
+  };
 
+
+
+  // คำสั่ง เพิ่ม item เข้าหน้าquodetail 
   async insertitemproduct(modal: any) {
-    var response;
     try {
 
-      var detail = { ...this.Activeitemproduct }; // ... เป็นคำสั่ง copy activeitemproduct 
-
-
+      var detail = { ...this.Activequodetail }; // ... เป็นคำสั่ง copy Activequodetail 
       detail.item = this.Activequotation.detail.length + 1;
       detail.amount = detail.qty * detail.unitprice;
       detail.vat = (detail.amount - detail.discount) * 0.07;
       detail.total = (detail.amount - detail.discount) + detail.vat;
-      console.log("detail : ", detail)
-      /* var item: any = {
-        itemproductcode: this.Activeitemproduct.itemproductcode ,
-        itemprice: this.Activeitemproduct.itemprice,
-        numunit: this.Activeitemproduct.numunit,
-        totalvalue: this.Activeitemproduct.totalvalue
-      }; */
-
-
       this.Activequotation.detail.push(detail);
+      this.Activequotation.amount += detail.amount;
+      this.Activequotation.vat += detail.vat;
+      this.Activequotation.total += detail.total;
 
-      console.log("Activequotation : ", this.Activequotation)
       modal.close();
 
     } catch (ex: any) {
 
     }
     return;
-  }
+  };
 
 }
